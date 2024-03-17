10bff00d0c04a3af617c24fb9a7f5840f76b7f0b
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-03-17 14:20:13 +1030

    Initial Commit

---
+++ CourseList/CourseListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseListApp.swift
+//  CourseList
+//
+//  Created by Larissa Nogueira da Rocha on 17/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct CourseListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
